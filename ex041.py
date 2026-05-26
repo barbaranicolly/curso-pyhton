ca23378b0cbfdb1797115d069f011e59f8f2dbfc
Bárbara <barbaranicolly@hotmail.com>
2026-05-26 10:31:57 +0100

    atualizando exercicios python

--- ex041.py
+++ ex041.py
@@ -1,25 +1,22 @@
-total = 0
-mil = 0
-menor = 0
-barato = ''
+total = totmil = menor = cont = 0
+barato = ' '
 while True:
     print('----LOJA SUPER BARATAO----')
     produto = input('digite o nome do produto: ')
-    preco = float(input('digite o preço do produto: '))
-    continuar = input('quer continuar? [S/N]: ').strip().upper()[0]
-    while continuar not in 'SN':
-        continuar = input('digite apenas S ou N: ').strip().upper()[0]
-    if continuar == 'S':
-        preco += total
-        total += preco
+    preco = float(input('digite o preço do produto : '))
+    cont += 1
+    total += preco
     if preco > 1000:
-         mil += 1
-    if menor == 0 or preco < menor:
+         totmil += 1
+    if cont == 1 or preco < menor:
          menor = preco
          barato = produto
-    elif continuar == 'N':
-        break
+    resp = ' '
+    while resp not in 'SN':
+        resp = input('quer continuar? [S/N]: ').strip().upper()[0]
+    if resp == 'N':
+            break
 print('----fim do programa----')
 print(f'o total gasto na compra foi de {total}')
-print(f' o produto mais barato foi o {barato}')
-print(f'produtos acima e 1000: {mil}')
+print(f' o produto mais barato foi o {barato} que custa {menor}')
+print(f'produtos acima e 1000: {totmil}')
